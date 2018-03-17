b7c1536ee26e351c4719ae13fef40ab27690bd83
Pulkit Gaur <32465084+pulkitg2196@users.noreply.github.com>
2018-03-17 15:55:14 +0530

    Update smartmeter.ino

--- smartmeter.ino
+++ smartmeter.ino
@@ -1,20 +1,26 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
-#define pulse 13
-int i,Pulse;
-float one_pulse=0.3125;
+#define pulse1 8
+#define pulse2 9
+#define pulse3 10
+#define pulse4 11
+int i;
+float one_pulse=0.03125;
 
 
 void setup()
 {
   Serial.begin(9600);
-pinMode(pulse, INPUT);
+pinMode(pulse1, INPUT);
+pinMode(pulse2, INPUT);
+pinMode(pulse3, INPUT);
+pinMode(pulse4, INPUT);
 lcd.begin(16,2);
 lcd.setCursor(0,0);
 lcd.print("SmartElectricity");
 lcd.setCursor(0,1);
-lcd.print("   Meter");
+lcd.print("     Meter");
 delay(4000);
 lcd.clear();
 
@@ -22,30 +28,48 @@ lcd.clear();
 
 void loop()
 {
-   int Pulse=0;
+   
  float Rupee=0,Unit=0;
  lcd.clear();
  while(1)
  {
-  if(digitalRead(pulse))
+  
+  if(digitalRead(pulse1)==1)
   {
    i++;
-   Pulse++;
-   Unit=one_pulse*i/10;                  // unit is defined
-   Rupee=Unit*10;                             // unit rate is defined
-   while(digitalRead(pulse));
+   Unit=one_pulse*i/10;    
+   Rupee=Unit*10;          
+  
   }
-
-
+  if(digitalRead(pulse2)==1)
+  {
+   i++;
+   Unit=one_pulse*i/10;    
+   Rupee=Unit*10;          
+   
+  }
+  if(digitalRead(pulse3)==1)
+  {
+   i++;   
+   Unit=one_pulse*i/10;                  
+   Rupee=Unit*10;                        
+   
+  }
+  if(digitalRead(pulse4)==1)
+  {
+   i++;   
+   Unit=one_pulse*i/10;                  
+   Rupee=Unit*10;                             
+   }
 lcd.setCursor(0,0);
-lcd.print("Rupee: ");
+lcd.print("Rupees: ");
 lcd.print(Rupee);
 lcd.setCursor(0,1);
 lcd.print("Unit: ");
 lcd.print(Unit);
 Serial.println(Unit);
-Serial.println("\n");
+//Serial.println("\n");
 Serial.println(Rupee);
-delay(10);
+delay(100);
 }
 }
